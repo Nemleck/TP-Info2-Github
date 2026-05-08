401b439bfb5cfddda7e3e47166a7cfded1f27991
Luna Bellanger <bellanger.aymeric@gmail.com>
2026-05-08 19:45:01 +0200

    Ajout de la documentation dans les fichiers 'circle.cpp' et 'circle.hpp' (En anglais pour la cohérence du code)

--- include/shapes/circle.hpp
+++ include/shapes/circle.hpp
@@ -7,18 +7,54 @@ struct Circle {
 	double radius;
 	Point center;
 	
+	/*Initializer (Circle)
+	Creates a new Circle instance.
+	@param r : type = double : the circle's radius
+	@param c : type = Point : the circle's center point
+	@return Circle : the circle of center point c and radius r
+	*/
 	Circle(double r, Point c = Point());
 	
+	/*circumference method (Circle)
+	Computes the circumference of the current circle.
+	@param: None
+	@return: double : the circle's circumference
+	*/
 	double circumference();
 	
+	/*area method (Circle)
+	Computes the area of the current circle.
+	@param: None
+	@return: double : the circle's area
+	*/
 	double area();
 	
+	/*draw method (Circle)
+	Draws the current circle using the draw_picture fubnction found in draw.hpp, itself using the CTurtle Library.
+	@param: None
+	@return: None
+	*/
 	void draw();
 	
+	/*translate method (Circle)
+	Operates a translation of the current circle by a vector (the center is moved by the vector).
+	@param: T : type = Point : the translation vector, implemented in the same way as points are.
+	@return: None
+	*/
 	void translate(Point T);
 	
-	void resize(double ratio); // resize while maintaining center
+	/*resize method (Circle)
+	Resizes the current circle by a ratio.
+	@param: ratio : type = double : the resize ratio.
+	@return: None
+	*/
+	void resize(double ratio);
 	
+	/*equals method (Circle)
+	Checks if the current circle is equal to another (same center point and same radius)
+	@param: ratio : type = double : the resize ratio.
+	@return: bool : true if they are equals, false otherwise
+	*/
 	bool equals(Circle circle);
 	
 };
